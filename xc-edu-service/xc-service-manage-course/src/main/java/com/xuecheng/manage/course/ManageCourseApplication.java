d10e97ee715d6d8f09c999e7fe372dbb45c6d431
闵继弘 <15995283349@163.com>
2020-01-19 23:12:29 +0800

    feign client test

--- xc-edu-service/xc-service-manage-course/src/main/java/com/xuecheng/manage/course/ManageCourseApplication.java
+++ xc-edu-service/xc-service-manage-course/src/main/java/com/xuecheng/manage/course/ManageCourseApplication.java
@@ -3,6 +3,7 @@ package com.xuecheng.manage.course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -16,6 +17,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages={"com.xuecheng.manage.course"})
 @ComponentScan(basePackages={"com.xuecheng.framework"})//扫描common下的所有类
 @EnableFeignClients
+@EnableDiscoveryClient
 public class ManageCourseApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ManageCourseApplication.class, args);
